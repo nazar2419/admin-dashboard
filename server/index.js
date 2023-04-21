4cb5ea9fe60dbd91c6e0476fa0f5addac2c3bd66
nazar2419 <nazarx095@gmail.com>
2023-04-21 16:02:17 +0300

    performance page

--- server/index.js
+++ server/index.js
@@ -18,12 +18,14 @@ import Product from './models/Product.js';
 import ProductStat from './models/ProductStat.js';
 import Transaction from './models/Transaction.js';
 import OverallStat from './models/OverallStat.js';
+import AffiliateStat from './models/AffiliateStat.js';
 import {
   dataUser,
   dataProduct,
   dataProductStat,
   dataTransaction,
   dataOverallStat,
+  dataAffiliateStat,
 } from './data/index.js';
 /* CONFIGURaTION */
 dotenv.config();
@@ -54,6 +56,7 @@ mongoose
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
     /* ONLY ADD DATA ONE TIME  */
+    //AffiliateStat.insertMany(dataAffiliateStat);
     // Product.insertMany(dataProduct);
     //ProductStat.insertMany(dataProductStat);
     // Transaction.insertMany(dataTransaction);
